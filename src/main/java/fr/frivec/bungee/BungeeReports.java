e4e5b92df121b34c70d7faf3c59c9d0b291bc18f
Frivec <frivec.99@gmail.com>
2020-06-17 20:53:51 +0200

    Added initialization of storage ways

--- src/main/java/fr/frivec/bungee/BungeeReports.java
+++ src/main/java/fr/frivec/bungee/BungeeReports.java
@@ -1,7 +1,9 @@
 package fr.frivec.bungee;
 
 import fr.frivec.bungee.config.Config;
+import fr.frivec.bungee.storage.Credentials;
 import fr.frivec.bungee.storage.database.Database;
+import fr.frivec.bungee.storage.files.ReportStorage;
 import fr.frivec.core.json.GsonManager;
 import fr.frivec.core.logger.Logger;
 import fr.frivec.core.logger.Logger.LogLevel;
@@ -15,6 +17,7 @@ public class BungeeReports extends Plugin {
 	private Config config;
 	private Logger logger;
 	private Database database;
+	private ReportStorage fileStorage;
 	private GsonManager gson;
 	
 	@Override
@@ -22,9 +25,29 @@ public class BungeeReports extends Plugin {
 		
 		instance = this;
 		
+		//Instanciate all the managers and the config
 		this.config = new Config();
 		this.logger = new Logger(this);
 		this.gson = new GsonManager();
+		
+		boolean sql = getConfig().getBoolean("MySQL.Enable"), file = getConfig().getBoolean("File.Enable");
+		
+		if(sql && file) {
+			
+			log(LogLevel.WARNING, "Error. Both of storage ways can't be active at the same time. Disabling the both.");
+			sql = false;
+			file = false;
+			
+		}
+			
+		if(sql)
+			
+			this.database = new Database(new Credentials(getConfig().getString("MySQL.Host"), getConfig().getString("MySQL."), 
+										getConfig().getString("MySQL.Password"), getConfig().getInt("MySQL.Port", 3006)));
+		
+		else if(file)
+			
+			this.fileStorage = new ReportStorage();
 		
 		super.onEnable();
 	}
@@ -55,9 +78,13 @@ public class BungeeReports extends Plugin {
 		return gson;
 	}
 	
+	public ReportStorage getFileStorage() {
+		return fileStorage;
+	}
+	
 	public static void log(final LogLevel level, final String message) {
 		
-		getInstance().logger.log(level, getInstance().getConfig().getString("prefix").replace("&", "§") + " " + message);
+		getInstance().logger.log(level, getInstance().getConfig().getString("prefix").replace("&", "§") + " " + level.getColorTag() + message);
 		
 	}			
 
